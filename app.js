0aa10fbeb9b101db2d08989eba2998ce17eb9dbf
Josh Noble <josh.noble13@gmail.com>
2019-02-11 22:45:50 -0500

    added jquery

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 /*http://callmenick.com/post/expanding-search-bar-using-css-transitions*/
 (function($) {
-    "use strict";
+  "use strict";
 
-    var $navbar = $(".nav"),
-        y_pos = $navbar.offset().top,
-        height = $navbar.height();
+  var $navbar = $(".nav"),
+    y_pos = $navbar.offset().top,
+    height = $navbar.height();
 
-    //scroll top 0 sticky
-    $(document).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        if (scrollTop > 0) {
-          $navbar.addClass("sticky");
-        } else {
-          $navbar.removeClass("sticky");
-        }
-    });
+  //scroll top 0 sticky
+  $(document).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop > 0) {
+      $navbar.addClass("sticky");
+    } else {
+      $navbar.removeClass("sticky");
+    }
+  });
 
-    //section sticky
-    /*$(document).scroll(function() {
+  //section sticky
+  /*$(document).scroll(function() {
         var scrollTop = $(this).scrollTop();
         if ($(window).height() > scrollTop) {
           $navbar.removeClass("sticky");
@@ -25,9 +25,8 @@
           $navbar.addClass("sticky");
         }
     });*/
-
 })(jQuery, undefined);
 
-$(".menu").click(function(){
+$(".menu").click(function() {
   $("#nav").toggleClass("open");
 });
