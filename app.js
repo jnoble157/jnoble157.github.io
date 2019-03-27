518ac249f6d227fb8a428826cfc69d3ba70dde88
Josh Noble <josh.noble13@gmail.com>
2019-03-26 21:18:47 -0400

    rough draft

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* Navigation Bar */
 /*http://callmenick.com/post/expanding-search-bar-using-css-transitions*/
 (function($) {
   "use strict";
@@ -29,4 +30,46 @@
 
 $(".menu").click(function() {
   $("#nav").toggleClass("open");
+});
+
+/* Contact Form */
+$(function() {
+  // init the validator
+  // validator files are included in the download package
+  // otherwise download from http://1000hz.github.io/bootstrap-validator
+
+  $("#contact-form").validator();
+
+  // when the form is submitted
+  $("#contact-form").on("submit", function(e) {
+    // if the validator does not prevent form submit
+    if (!e.isDefaultPrevented()) {
+      var url = "contact.php";
+
+      // FOR CODEPEN DEMO I WILL PROVIDE THE DEMO OUTPUT HERE, download the PHP files from
+      // https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
+
+      var messageAlert = "alert-success";
+      var messageText =
+        "Your message was sent, thank you. I will get back to you soon.";
+
+      // let's compose Bootstrap alert box HTML
+      var alertBox =
+        '<div class="alert ' +
+        messageAlert +
+        ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+        messageText +
+        "</div>";
+
+      // If we have messageAlert and messageText
+      if (messageAlert && messageText) {
+        // inject the alert to .messages div in our form
+        $("#contact-form").find(".messages").html(alertBox);
+        // empty the form
+        $("#contact-form")[0].reset();
+      }
+
+      return false;
+    }
+  });
 });
